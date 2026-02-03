ae7057f5c2b008c0b9ef78caf0fbc4c28a132462
wyatts97 <wyattlonerboy97@gmail.com>
2026-02-03 00:32:30 -0600

    test

--- config/app.php
+++ config/app.php
@@ -20,7 +20,4 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
-    'providers' => [
-        App\Providers\Filament\AdminPanelProvider::class,
-    ],
 ];
