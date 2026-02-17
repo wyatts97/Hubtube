e9bfe8202342ea16b91d7bfd246b06c34a67a344
wyatts97 <wyattlonerboy97@gmail.com>
2026-02-17 02:53:00 -0600

    rm

--- fix_db.sql
+++
@@ -1,4 +0,0 @@
-DROP USER IF EXISTS 'hubtube'@'localhost';
-CREATE USER 'hubtube'@'localhost' IDENTIFIED BY 'MjgQdPorMf';
-GRANT ALL PRIVILEGES ON hubtube.* TO 'hubtube'@'localhost';
-FLUSH PRIVILEGES;
