240bc3e903b077d98af4df4cfd82d4a816a5e4d5
wyatts97 <wyattlonerboy97@gmail.com>
2026-02-27 21:55:00 -0600

    Update providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,6 +5,7 @@ return [
     App\Providers\AuthServiceProvider::class,
     App\Providers\DynamicConfigServiceProvider::class,
     App\Providers\EventServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\SocialLoginServiceProvider::class,
 ];
