df3891b1c3d96b599ddd886d6f5e6158fed2483f
wyatts97 <wyattlonerboy97@gmail.com>
2026-07-31 04:43:19 -0500

    Update backup.php

--- config/backup.php
+++ config/backup.php
@@ -67,6 +67,7 @@ return [
             'keep_daily_backups_for_days' => 16,
             'keep_weekly_backups_for_weeks' => 8,
             'keep_monthly_backups_for_months' => 4,
+            'keep_yearly_backups_for_years' => 2,
             'delete_oldest_backups_when_using_more_megabytes_than' => 5000,
         ],
     ],
