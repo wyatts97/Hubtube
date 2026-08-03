ea0ae90ef80b03ff20ad48f3d514c81f86074249
wyatts97 <wyattlonerboy97@gmail.com>
2026-08-03 18:46:16 -0500

    Update Logs.php

--- app/Filament/Pages/Logs.php
+++ app/Filament/Pages/Logs.php
@@ -6,9 +6,9 @@ use LaBoiteACode\FilamentLogsExplorer\Pages\LogsExplorer as BaseLogsExplorer;
 
 class Logs extends BaseLogsExplorer
 {
-    protected static ?string $navigationLabel = 'Logs';
+    protected static string|\BackedEnum|null $navigationLabel = 'Logs';
 
-    protected static ?string $navigationIcon = 'heroicon-o-document-magnifying-glass';
+    protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-document-magnifying-glass';
 
     protected static string|\UnitEnum|null $navigationGroup = 'Tools';
 
